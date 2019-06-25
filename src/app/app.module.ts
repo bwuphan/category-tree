6445fdca91a679b89c4a66b1299c22049075965b
bwuphan <bwuphan@gmail.com>
2019-06-24 23:58:58 -0700

    Remove node-modal and tree components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TreeComponent } from './tree/tree.component';
 import { NodeComponent } from './node/node.component';
-import { AddNodeModalComponent } from './add-node-modal/add-node-modal.component';
 import { NodeChildrenComponent } from './node-children/node-children.component';
 import { NodeFormComponent } from './node-form/node-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TreeComponent,
     NodeComponent,
-    AddNodeModalComponent,
     NodeChildrenComponent,
     NodeFormComponent
   ],
