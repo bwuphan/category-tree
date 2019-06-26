5b5b4ce08422233c42ca743936e9da1cf1fcc3a3
bwuphan <bwuphan@gmail.com>
2019-06-25 20:36:54 -0700

    Add ReactiveFormsModule to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NodeComponent } from './node/node.component';
@@ -16,7 +16,8 @@ import { NodeFormComponent } from './node-form/node-form.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
