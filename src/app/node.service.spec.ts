41d2a8a3af76fa7d2525f8c344483e7b21d8b040
bwuphan <bwuphan@gmail.com>
2019-06-25 20:37:27 -0700

    Add tests for removeChild to node.service.spec.ts

--- src/app/node.service.spec.ts
+++ src/app/node.service.spec.ts
@@ -10,7 +10,7 @@ describe('NodeService', () => {
     expect(service).toBeTruthy();
   });
 
-  describe('addNode()', () => {
+  describe('addChild(child, parent)', () => {
     it('should add a child node to the parent', () => {
       const service: NodeService = TestBed.get(NodeService);
 
@@ -24,10 +24,59 @@ describe('NodeService', () => {
         children: []
       };
 
-      service.addNode(childNode, parentNode);
+      service.addChild(childNode, parentNode);
 
       expect(parentNode.children.length).toBe(1);
       expect(JSON.stringify(parentNode.children[0])).toBe(JSON.stringify(childNode));
+    });
+  });
+
+  describe('removeChild(index, parent)', () => {
+    it('should remove the child if index is in bounds and there is only one child', () => {
+      const service: NodeService = TestBed.get(NodeService);
+
+      const parentNode: Node = {
+       name: 'parent',
+       children: [new Node()]
+      };
+
+      service.removeChild(0, parentNode);
+
+      expect(parentNode.children.length).toBe(0);
+    });
+
+    it('should remove the correct child if there are multiple children', () => {
+      const service: NodeService = TestBed.get(NodeService);
+
+      const parentNode: Node = {
+       name: 'parent',
+       children: [new Node('one'), new Node('two'), new Node('three')]
+      };
+
+      service.removeChild(1, parentNode);
+
+      expect(parentNode.children.length).toBe(2);
+      expect(parentNode.children[0].name).toBe('one');
+      expect(parentNode.children[1].name).toBe('three');
+
+      service.removeChild(1, parentNode);
+      expect(parentNode.children.length).toBe(1);
+      expect(parentNode.children[0].name).toBe('one');
+    });
+
+    it('should not remove any children if the index is out of bounds', () => {
+      const service: NodeService = TestBed.get(NodeService);
+
+      const parentNode: Node = {
+       name: 'parent',
+       children: [new Node('one'), new Node('two'), new Node('three')]
+      };
+
+      service.removeChild(-1, parentNode);
+      expect(parentNode.children.length).toBe(3);
+
+      service.removeChild(4, parentNode);
+      expect(parentNode.children.length).toBe(3);
     });
   });
 });
